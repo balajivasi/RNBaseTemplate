90918242b170b52b9424780e0bc1b9e62806338a
BALAJI VASI <balajivasi@gmail.com>
2024-03-07 17:56:44 -0500

    tailwind added working fine in android need to check ios build issue.

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Main = () => {
+    return (
+        <View>
+            <Text className="text-red-500">Main</Text>
+        </View>
+    )
+}
+
+export default Main
